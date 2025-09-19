0c69dd4ee040389d314e9b9bfb93b4bacb677ca9
shawn miner <shawnmin13@gmail.com>
2025-09-19 13:54:51 -0600

    Classes Created

--- BudgetApp.Logic/Catagory.cs
+++ BudgetApp.Logic/Catagory.cs
@@ -1,5 +1,12 @@
 ﻿namespace BudgetApp.Logic;
 public class Catagory
 {
-
+    List<Item> items;
+    private int Budget { get; set; }
+    
+    public Catagory(int budget)
+    {
+               items = new();
+        Budget = budget;
+    }
 }
--- BudgetApp.Logic/Item.cs
+++ BudgetApp.Logic/Item.cs
@@ -1,5 +1,7 @@
 ﻿namespace BudgetApp.Logic;
 public class Item
 {
-
+    private string name { get; set; }
+    private decimal price { get; set; }
+    private bool isFoodItem { get; set; }  
 }
